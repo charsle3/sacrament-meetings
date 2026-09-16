b403b6a95e18439929e61c988f7faf2dc260daa0
charsle3 <jpkingthe3rd@gmail.com>
2026-09-16 02:01:05 -0700

    test try catch

--- app/meetings/page.tsx
+++ app/meetings/page.tsx
@@ -14,7 +14,7 @@ export default async function Meetings() {
             throw new Error('Failed to fetch meetings');
         }
 
-        const meetings: SacramentMeeting[] = JSON.parse(await response.text());
+        const meetings: SacramentMeeting[] = await response.json();
     
 
     // const meetings: SacramentMeeting[] = getMeetings();
